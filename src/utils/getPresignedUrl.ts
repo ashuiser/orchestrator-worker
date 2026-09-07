8708675ab5808104852537d06a704e71b4ee0209
Ashutosh Panigrahi <ashuistic@gmail.com>
2026-09-08 01:00:25 +0530

    Add caching to presignedUrl generator to prevent recreating signedUrl before expiration of previous one.

--- src/utils/getPresignedUrl.ts
+++ src/utils/getPresignedUrl.ts
@@ -1,6 +1,7 @@
 import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { env } from "../config/env.js";
+import redisClient from "../config/redis.js";
 
 const s3 = new S3Client({
 	region: env.S3_REGION,
@@ -12,10 +13,17 @@ const s3 = new S3Client({
 });
 
 export async function getPresignedUrl(srcKey: string) {
+	const cachedUrl = await redisClient.get(`fotto:cache:presignedurl:${srcKey}`);
+	if (cachedUrl) {
+		return cachedUrl;
+	}
 	const getUrl = await getSignedUrl(
 		s3,
 		new GetObjectCommand({ Bucket: env.RAW_BUCKET_NAME, Key: srcKey }),
 		{ expiresIn: 3600 }, // Valid for 1 hour
 	);
+	await redisClient.set(`fotto:cache:presignedurl:${srcKey}`, getUrl, {
+		EX: 3600,
+	});
 	return getUrl;
 }
